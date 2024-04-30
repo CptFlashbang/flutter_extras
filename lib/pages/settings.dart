085c0bb088c0e971f14c17fc1d5f698f8e6e8679
CptFlashbang <Callum.yates.180@gmail.com>
2024-04-30 15:50:48 +0100

    added permissions checks

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -17,15 +17,91 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-       body: Container(
+      body: Container(
         margin: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[ Text("Settings"),
+          children: <Widget>[
+            const Text("Settings"),
+            ElevatedButton(
+              onPressed: requestCameraPermission,
+              child: const Text(
+                "Request Camera Permission",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: requestMultiplePermissions,
+              child: const Text(
+                "Request Multiple Permissions",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: requestPermissionWithOpenSettings,
+              child: const Text(
+                "Open Permission Settings",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  void requestCameraPermission() async {
+    /// status can either be: granted, denied, restricted or permanentlyDenied
+    var status = await Permission.camera.status;
+    if (status.isGranted) {
+      print("Camera Permission is already there");
+    }
+    else if (status.isDenied) {
+      // We didn't ask for permission yet or the permission has been denied before but not permanently.
+      if (await Permission.camera.request().isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        print("Camera Permission is granted");
+      }
+    }
+
+    // You can also directly ask the permission about its status.
+    // if (await Permission.location.isRestricted) {
+    //   // The OS restricts access, for example because of parental controls.
+    // }
+  }
+
+  /// Request multiple permissions at once.
+  /// In this case location & storage
+  void requestMultiplePermissions() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.location,
+      Permission.storage,Permission.contacts
+    ].request();
+    print("location permission is requested: ${statuses[Permission.location]}, "
+        "storage permission is requested: ${statuses[Permission.storage]}, "
+        "contacts permission is requested: ${statuses[Permission.contacts]}");
+  }
+
+  /// The user opted to never again see the permission request dialog for this
+  /// app. The only way to change the permission's status now is to let the
+  /// user manually enable it in the system settings.
+  void requestPermissionWithOpenSettings() async {
+    //if (await Permission.speech.isPermanentlyDenied) {
+      openAppSettings();
+    //}
+  }
 }
+
