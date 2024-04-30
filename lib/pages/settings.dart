5534a18409d5c0fb04ea582e655ed4f70fbdd03e
CptFlashbang <Callum.yates.180@gmail.com>
2024-04-30 15:17:37 +0100

    added permission handler

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
