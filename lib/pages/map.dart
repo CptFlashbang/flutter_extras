54ae0ba00fea8372bfd5d0f85cb46f3d85a34860
CptFlashbang <Callum.yates.180@gmail.com>
2024-04-30 16:40:23 +0100

    add google maps dependency

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 class MapPage extends StatefulWidget {
   const  MapPage({Key? key}) : super(key: key);
@@ -10,21 +14,96 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+ Position? _currentPosition;
+  String _currentAddress = "";
+ Map<String, double>? userLocation;
+  var placeArray = [];
+  String dispLocation = "";
+  String dispCurrent = "";
+
+  _setLastLocation(lat,long,address) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('lat', lat);
+    await prefs.setDouble('long', long);
+    await prefs.setString('address', address);
+    setState(() {
+      dispLocation = '';
+    });
+  }
+
+  _getLastLocation() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      dispLocation = 'Last coordinates LAT: ${prefs.getDouble('lat')}, LNG: ${prefs.getDouble('long')}, Address: ${prefs.getString('address')}';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Map"),
+        title: const Text("Home"),
       ),
       body: Container(
         margin: const EdgeInsets.all(10.0),
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[ Text("Map"),
+          children: <Widget>[
+            ElevatedButton(
+                child: const Text("Get location"),
+                onPressed: () {
+                  _getCurrentLocation();
+                }),
+              Text(dispCurrent),
+              if (_currentPosition != null) ElevatedButton(
+                child: const Text("Save current location"),
+                onPressed: () {
+                  _setLastLocation(_currentPosition?.latitude,_currentPosition?.longitude,_currentAddress);
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Saved LAT: ${_currentPosition?.latitude}, LNG: ${_currentPosition?.longitude}')));
+                }),
+            ElevatedButton(
+                child: const Text("Get saved location"),
+                onPressed: () {
+                  _getLastLocation();
+                }),
+            Text(dispLocation),
           ],
         ),
       ),
     );
+  }
+
+  _getCurrentLocation() async {
+    // LocationPermission permission = await Geolocator.requestPermission();
+    Geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best, forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+       _getAddressFromLatLng();
+      });
+      }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        // 52.815310,-2.214990
+        _currentPosition!.latitude,
+          _currentPosition!.longitude 
+      );
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress = "${place.street}, ${place.locality}, ${place.subAdministrativeArea}, ${place.country}, ${place.postalCode}";
+       dispCurrent = "LAT: ${_currentPosition?.latitude}, LNG: ${_currentPosition?.longitude}, current address is $_currentAddress";
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
