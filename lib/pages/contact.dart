9491a45b9ca72f31da85f2578fe9aceb5c7bdcbd
CptFlashbang <Callum.yates.180@gmail.com>
2024-04-30 18:01:10 +0100

    added contact retrieval

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactPage extends StatefulWidget {
   const  ContactPage({Key? key}) : super(key: key);
@@ -10,21 +11,60 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+List<Contact>? _contacts;
+  bool _permissionDenied = false;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Contact"),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[ Text("Contact"),
-          ],
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    _fetchContacts();
+  }
+
+  Future _fetchContacts() async {
+    if (!await FlutterContacts.requestPermission(readonly: true)) {
+      setState(() => _permissionDenied = true);
+    } else {
+      final contacts = await FlutterContacts.getContacts();
+      setState(() => _contacts = contacts);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('flutter_contacts_example')),
+          body: _body()));
+
+  Widget _body() {
+    if (_permissionDenied) return const Center(child: Text('Permission denied'));
+    if (_contacts == null) return const Center(child: CircularProgressIndicator());
+    return ListView.builder(
+        itemCount: _contacts!.length,
+        itemBuilder: (context, i) => ListTile(
+            title: Text(_contacts![i].displayName),
+            onTap: () async {
+              final fullContact =
+                  await FlutterContacts.getContact(_contacts![i].id);
+              await Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => ContactSubPage(fullContact!)));
+            }));
   }
 }
+
+class ContactSubPage extends StatelessWidget {
+  final Contact contact;
+  ContactSubPage(this.contact);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: Text(contact.displayName)),
+      body: Column(children: [
+        Text('First name: ${contact.name.first}'),
+        Text('Last name: ${contact.name.last}'),
+        Text(
+            'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
+        Text(
+            'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
+      ]));
+}
+
